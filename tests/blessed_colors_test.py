c37c809b799425acab89691b24e62bdd4e1f372f
Dante Mendoza Leyva <40130428+Apocryphon-X@users.noreply.github.com>
2021-03-18 02:10:39 -0600

    Adding more tests

--- tests/blessed_colors_test.py
+++ tests/blessed_colors_test.py
@@ -1,6 +1,10 @@
 from blessed import Terminal
 
-print(Terminal().red("hello"))
-print(Terminal().green("hello"))
-print(Terminal().blue("hello"))
-print(Terminal().yellow("hello"))
+print(Terminal().red("Testing Color..."))
+print(Terminal().green("Testing Color..."))
+print(Terminal().blue("Testing Color..."))
+print(Terminal().yellow("Testing Color..."))
+
+print(Terminal().orangered("Testing Color..."))
+print(Terminal().limegreen("Testing Color..."))
+print(Terminal().skyblue("Testing Color..."))
