227e6d3911668db07b115b0ce25171cdafb53775
Dante Mendoza Leyva <apocryphon.x.contact@gmail.com>
2021-03-18 21:43:32 -0600

    Update version in output

--- tests/sample_code.cpp
+++ tests/sample_code.cpp
@@ -1,4 +1,4 @@
-// Sent using OmegaUp CLI 0.5.0-alpha
+// Sent using OmegaUp CLI 0.0.6-alpha
 // https://github.com/Apocryphon-X/omegaup-cli
 
 #include <iostream>
@@ -6,6 +6,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Say hi to the OmegaUp CLI! 0.5.0a\n";
+    cout << "Say hi to the OmegaUp CLI! 0.0.6a\n";
     return 0;
 }
