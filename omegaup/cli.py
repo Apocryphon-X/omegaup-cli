0162f09177f3b00b1c1958cd1073aeebb8110611
Dante Mendoza Leyva <40130428+Apocryphon-X@users.noreply.github.com>
2021-03-18 20:50:29 -0600

    maybe just a dot line

--- omegaup/cli.py
+++ omegaup/cli.py
@@ -69,9 +69,6 @@ def follow_submit(target_session, run_guid):
             print(".", end = "")
             time.sleep(0.5)
 
-        time.sleep(2)
-        print("\r" + blessed.Terminal().clear_eol, end = "")
-    
     if json_response["status"] == "ready":
         if json_response["verdict"] == "AC" : print(ac_verdict) 
         if json_response["verdict"] == "WA" : print(wa_verdict)
