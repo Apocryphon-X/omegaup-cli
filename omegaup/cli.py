22dd764bdd73eba23a8e03bef0ba03b03cac67d9
Dante Mendoza Leyva <40130428+Apocryphon-X@users.noreply.github.com>
2021-03-20 20:44:59 -0600

    Testing with another problem

--- omegaup/cli.py
+++ omegaup/cli.py
@@ -124,7 +124,7 @@ def main():
                     with cli_terminal.hidden_cursor():
                         follow_submit(main_session, submit_guid)
     elif "test" in sys.argv: 
-        setup_lab(main_session, "comi-a-b")
+        setup_lab(main_session, "Los-locos-que-hacen-tick-tocks")
 
 
 if __name__ == "__main__":
